a336c17a5da3ef4ecdcee9910ffae71b95df49fd
Fernando <jamitamipapito@gmail.com>
2020-08-10 11:54:07 -0600

    ADD tipo usuario

--- SCRIPTS/ADD_TIPOUSUARIO.sql
+++ SCRIPTS/ADD_TIPOUSUARIO.sql
@@ -2,6 +2,7 @@ CREATE PROCEDURE `ADD_TIPOUSUARIO`(
 IN _DescripdionTipoUsuario varchar(45), OUT _LID int
 )
 BEGIN
+
 insert into TipoUsuario(DescripcionTipoUsuario)values(_DescripdionTipoUsuario);
 SET _LID= last_insert_id();
 SELECT * FROM TipoUsuario WHERE IdTipoUsuario= _LID;
